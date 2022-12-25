30347bf0b0073b0fe779dcd5990a25c265fb86ae
Malay <malaykumar2003@gmail.com>
2022-12-25 21:13:29 +0530

    Frontend and Backend

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(() => ({
+  esbuild: {
+    loader: "jsx",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+        ".ts": "tsx",
+      },
+    },
+  },
+  plugins: [react()],
+}));
