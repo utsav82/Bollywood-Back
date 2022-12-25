2826dc4759449aec3c312a71c84f3d5bcf1bb467
utsav82 <utsa.yadav.94@gmail.com>
2022-12-25 19:43:06 +0530

    added yt search

--- Server/tmdb.js
+++ Server/tmdb.js
@@ -4,7 +4,7 @@ export function search(date1, date2){
     return axios.get(urlMovie + 'discover/movie',
         {
             params: {
-                'api_key': process.env.API_KEY,
+                'api_key': process.env.API_KEY_TMDB,
                 'sort_by': 'popularity.desc',
                 'with_original_language': 'hi',
                 'primary_release_date.gte': date1,
@@ -14,12 +14,18 @@ export function search(date1, date2){
 
     )
 }
-export function trailer(id)
+export function trailer(name)
 {
-    return axios.get(urlMovie + 'movie/' + id +'/videos',
+    return axios.get('https://www.googleapis.com/youtube/v3/search',
     {
         params: {
-            'api_key': process.env.API_KEY,
+            'key': process.env.API_KEY_YT,
+            'type': 'video',
+            'part': 'snippet',
+            'maxResults': 5,
+            'q': name
+           
+
         }
     }
 
