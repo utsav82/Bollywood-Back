ba94d651c18d35f4e821c99f5a1790576da738b7
utsav82 <utsa.yadav.94@gmail.com>
2023-05-21 03:53:01 +0530

    Removed checks

--- Server/index.js
+++ Server/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 8080 ;
-  
+app.use(express.static(path.join(__dirname, '../Client', 'dist')));
 app.get("/movie", async (req, res) => {
   console.log(req.query.date);
   try {
@@ -78,18 +78,11 @@ app.get("/video", async (req, res) => {
   }
 });
 
-
-
-
-
-
-if(process.env.NODE_ENV === 'production')
-{
-  app.use(express.static(path.join(__dirname, '../Client', 'dist')));
+ 
   app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../Client', 'dist', 'index.html'));
   })
-}
+
 app.listen(PORT, () => {
   console.log(`Server Running on port- ${PORT}`);
 });
